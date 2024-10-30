b26bbd5950a2c703721fb0cb64cbc72cf49a2aea
Azis Kurniawan <156058504+Aziskurniawan200@users.noreply.github.com>
2024-10-30 09:27:27 +0700

    first
    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,76 +1,75 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import "./index.css"
-import DashboardPage from "./components/pages/DashboardPage"
-import HomeSchoolingKakSetoPage from "./components/pages/HomeSchoolingKakSetoPage"
-import KakSetoSchoolPage from "./components/pages/KakSetoSchoolPage"
-import SekolahKhususKakSetoPage from "./components/pages/SekolahKhususKakSetoPage"
-import KakSetoLearningCenterPage from "./components/pages/KakSetoLearningCenterPage"
-import { HelmetProvider } from "react-helmet-async"
-import HomePage from "./components/pages/undangan/HomePage"
-import AbsenPage from "./components/pages/undangan/AbsenPage"
-import SalamPage from "./components/pages/undangan/SalamPage"
-import LinktreePage from "./components/pages/LinktreePage"
-import LokasiPage from "./components/pages/undangan/LokasiPage"
-import InfoPage from "./components/pages/undangan/InfoPage"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import DashboardPage from './components/pages/DashboardPage';
+import HomeSchoolingKakSetoPage from './components/pages/HomeSchoolingKakSetoPage';
+import KakSetoSchoolPage from './components/pages/KakSetoSchoolPage';
+import SekolahKhususKakSetoPage from './components/pages/SekolahKhususKakSetoPage';
+import KakSetoLearningCenterPage from './components/pages/KakSetoLearningCenterPage';
+import { HelmetProvider } from 'react-helmet-async';
+import HomePage from './components/pages/undangan/HomePage';
+import AbsenPage from './components/pages/undangan/AbsenPage';
+import SalamPage from './components/pages/undangan/SalamPage';
+import LinktreePage from './components/pages/LinktreePage';
+import LokasiPage from './components/pages/undangan/LokasiPage';
+import InfoPage from './components/pages/undangan/InfoPage';
 
-const BASE_URL = "http://192.168.207.190:5173/"
-
+const BASE_URL = 'http://192.168.207.190:5173/';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <DashboardPage />,
   },
   {
-    path: "/HSKS",
+    path: '/HSKS',
     element: <HomeSchoolingKakSetoPage />,
   },
   {
-    path: "/KSS",
+    path: '/KSS',
     element: <KakSetoSchoolPage />,
   },
   {
-    path: "/SKKS",
+    path: '/SKKS',
     element: <SekolahKhususKakSetoPage />,
   },
   {
-    path: "/KSLC",
+    path: '/KSLC',
     element: <KakSetoLearningCenterPage />,
   },
   {
-    path: "/linktree",
+    path: '/linktree',
     element: <LinktreePage />,
   },
   {
-    path: "/undangan",
+    path: '/undangan',
     element: <HomePage />,
   },
   {
-    path: "/undangan/lokasi",
+    path: '/undangan/lokasi',
     element: <LokasiPage />,
   },
   {
-    path: "/undangan/info",
+    path: '/undangan/info',
     element: <InfoPage />,
   },
   {
-    path: "/undangan/qrcode",
+    path: '/undangan/qrcode',
     element: <AbsenPage />,
   },
   {
-    path: "/undangan/salam",
+    path: '/undangan/salam',
     element: <SalamPage />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <RouterProvider router={router} />
     </HelmetProvider>
   </React.StrictMode>
-)
+);
 
-export default BASE_URL
+export default BASE_URL;
